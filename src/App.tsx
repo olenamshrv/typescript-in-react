9c14a21378a6427295fb98e4153f7b826caa44cf
olena.mashurova <olena.mashurova@gmail.com>
2023-10-24 16:19:59 +0300

    upd typescript-in-react

--- src/App.tsx
+++ src/App.tsx
@@ -121,9 +121,142 @@
 
 
 
-//=========3====useContext====================================
+// //=========3====useContext====================================
 
-import React, { useContext, useReducer } from 'react';
+// import React, { useContext, useReducer } from 'react';
+// import './App.css';
+// import { ProductContext } from '.';
+
+// type InitialState = {
+//   count: 0;
+// };
+
+// type State = {
+//   count: number;
+// };
+
+// type Action = {
+//   type: ACTION_TYPE;
+// };
+
+// enum ACTION_TYPE {
+//   INCREMENT = "INCREMENT",
+//   DECREMENT = "DECREMENT",
+// }
+
+// const stateReducer: React.Reducer<State, Action>=(
+//   state: State,
+//   action: Action
+// ): State => {
+//   switch (action.type) {
+//     case ACTION_TYPE.INCREMENT:
+//       return {...state, count: state.count + 1};
+//     case ACTION_TYPE.DECREMENT:
+//       return {...state, count: state.count -1};
+//     default: return state;
+//   }
+// };
+
+// const App: React.FC<{}> = () => {
+//   const [state, dispatch] = useReducer<
+//       React.Reducer<State, Action>, 
+//       InitialState
+//     >(stateReducer, 
+//       { count: 0},
+//       (state: InitialState): State => ({
+//         ...state,
+//         count: 1,
+//       }));
+  
+//   const handleClick = () => {
+//     dispatch({type: ACTION_TYPE.INCREMENT})
+//   }
+
+//   const context = useContext(ProductContext);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p onClick={handleClick}>Count: {state.count}</p>
+//         <p>Selected product id: {context?.selectedProductId}</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// //=========4====useRef====================================
+
+// import React, { useContext, useReducer, useRef } from 'react';
+// import './App.css';
+// import { ProductContext } from '.';
+
+// type InitialState = {
+//   count: 0;
+// };
+
+// type State = {
+//   count: number;
+// };
+
+// type Action = {
+//   type: ACTION_TYPE;
+// };
+
+// enum ACTION_TYPE {
+//   INCREMENT = "INCREMENT",
+//   DECREMENT = "DECREMENT",
+// }
+
+// const stateReducer: React.Reducer<State, Action>=(
+//   state: State,
+//   action: Action
+// ): State => {
+//   switch (action.type) {
+//     case ACTION_TYPE.INCREMENT:
+//       return {...state, count: state.count + 1};
+//     case ACTION_TYPE.DECREMENT:
+//       return {...state, count: state.count -1};
+//     default: return state;
+//   }
+// };
+
+// const App: React.FC<{}> = () => {
+//   const [state, dispatch] = useReducer<
+//       React.Reducer<State, Action>, 
+//       InitialState
+//     >(stateReducer, 
+//       { count: 0},
+//       (state: InitialState): State => ({
+//         ...state,
+//         count: 1,
+//       }));
+  
+//   const handleClick = () => {
+//     dispatch({type: ACTION_TYPE.INCREMENT})
+//   }
+
+//   const context = useContext(ProductContext);
+
+//   const ref = useRef<HTMLElement | null>(null);
+
+//   return (
+//     <div className="App">
+//       <header ref={ref} className="App-header">
+//         <p onClick={handleClick}>Count: {state.count}</p>
+//         <p>Selected product id: {context?.selectedProductId}</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//=========4====useCallback====================================
+
+import React, { useContext, useReducer, useRef } from 'react';
 import './App.css';
 import { ProductContext } from '.';
 
@@ -174,16 +307,19 @@ const App: React.FC<{}> = () => {
 
   const context = useContext(ProductContext);
 
+  const ref = useRef<HTMLElement | null>(null);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header ref={ref} className="App-header">
         <p onClick={handleClick}>Count: {state.count}</p>
-        <p>Selected product id: {context.selectedProductId}</p>
+        <p>Selected product id: {context?.selectedProductId}</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
 
 
