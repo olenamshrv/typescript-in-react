5d05793195174ab3d8f3244b39254b97524c9819
olena.mashurova <olena.mashurova@gmail.com>
2023-10-24 15:11:47 +0300

    typescript in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,189 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//===1========useState==useEffect====================
 
-function App() {
+// import React from 'react';
+// import './App.css';
+// import { JsxElement } from 'typescript';
+
+// import { useState, useEffect } from 'react';
+
+// // const test: JSX.Element=<div>Test</div>;
+
+// // const test =<div>Test</div>;
+
+// type AppProps={
+//   test?: JSX.Element;
+//   // children: React.ReactNode;
+//   children: React.ReactElement;
+//   onClick: React.MouseEventHandler;
+// }
+
+// const App: React.FC<AppProps> = ({test, children, onClick}) => {
+//   const [count,setCount] = useState<number>(0);
+
+//   // const handleClick: React.MouseEventHandler<HTMLParagraphElement> =()=> {
+//   //   onClick();
+//   //   setCount((value)=> value+1);
+//   // }
+
+
+//   const handleClick = (event: React.MouseEvent<Element, MouseEvent>) => {
+//     setCount((value)=> value+1);
+//   }
+
+//   const handleAuxClick: React.MouseEventHandler<HTMLElement> = (event) => {}
+
+//   useEffect(()=>{
+//     alert(count);
+//   }, [count]);
+    
+//   return (
+//     <div className="App">
+//       <header onAuxClick={handleAuxClick} className="App-header">
+//         {/* <p onClick={handleClick}>Count: {count}</p> */}
+//         <p onClick={handleClick}>Count: {count}</p>
+//         <p>{test}</p>
+//         {children}
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// //=========2=======useReducer=================================
+
+// import React, { useReducer } from 'react';
+// import './App.css';
+
+// type InitialState = {
+//   count: 0;
+// };
+
+// type State = {
+//   count: number;
+// };
+
+// type Action = {
+//   type: ACTION_TYPE;
+// };
+
+// enum ACTION_TYPE {
+//   INCREMENT = "INCREMENT",
+//   DECREMENT = "DECREMENT",
+// }
+
+// const stateReducer: React.Reducer<State, Action>=(
+//   state: State,
+//   action: Action
+// ): State => {
+//   switch (action.type) {
+//     case ACTION_TYPE.INCREMENT:
+//       return {...state, count: state.count + 1};
+//     case ACTION_TYPE.DECREMENT:
+//       return {...state, count: state.count -1};
+//     default: return state;
+//   }
+// };
+
+// const App: React.FC<{}> = () => {
+//   // const initState: InitialState={ count: 0 };
+  
+//   // const initializer = (state: InitialState): State => ({
+//   //   ...state,
+//   //   count: 1,
+//   // });
+
+//   const [state, dispatch] = useReducer<
+//       React.Reducer<State, Action>, 
+//       InitialState
+//     >(stateReducer, 
+//       // initState, 
+//       { count: 0},
+//       (state: InitialState): State => ({
+//         ...state,
+//         count: 1,
+//       }));
+  
+//   const handleClick = ()=> {
+//     dispatch({type: ACTION_TYPE.INCREMENT})
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p onClick={handleClick}>Count: {state.count}</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+//=========3====useContext====================================
+
+import React, { useContext, useReducer } from 'react';
+import './App.css';
+import { ProductContext } from '.';
+
+type InitialState = {
+  count: 0;
+};
+
+type State = {
+  count: number;
+};
+
+type Action = {
+  type: ACTION_TYPE;
+};
+
+enum ACTION_TYPE {
+  INCREMENT = "INCREMENT",
+  DECREMENT = "DECREMENT",
+}
+
+const stateReducer: React.Reducer<State, Action>=(
+  state: State,
+  action: Action
+): State => {
+  switch (action.type) {
+    case ACTION_TYPE.INCREMENT:
+      return {...state, count: state.count + 1};
+    case ACTION_TYPE.DECREMENT:
+      return {...state, count: state.count -1};
+    default: return state;
+  }
+};
+
+const App: React.FC<{}> = () => {
+  const [state, dispatch] = useReducer<
+      React.Reducer<State, Action>, 
+      InitialState
+    >(stateReducer, 
+      { count: 0},
+      (state: InitialState): State => ({
+        ...state,
+        count: 1,
+      }));
+  
+  const handleClick = () => {
+    dispatch({type: ACTION_TYPE.INCREMENT})
+  }
+
+  const context = useContext(ProductContext);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={handleClick}>Count: {state.count}</p>
+        <p>Selected product id: {context.selectedProductId}</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,81 @@
+//======1===========useState======useEffect=====================
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// // const Child=()=><div>Child prop</div>;
+
+// const Child: React.FC<{ style: React.CSSProperties }> =({style})=><div style={style}>Child prop</div>;
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App onClick={()=> alert('123')} test={<div>Hello world</div>}>
+//       <Child style={{color: "red"}}/>
+//     </App>
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+// //=======2======useReducer================================
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+//=======3===useContext==============================
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createContext } from 'vm';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+type ProductContextType = {
+  selectedProductId: number;
+};
+
+export const ProductContext = createContext<ProductContextType | null>(null);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ProductContext.Provider value={{ selectedProductId: 1}}>
+      <App />
+    </ProductContext.Provider>
   </React.StrictMode>
 );
 
@@ -17,3 +83,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
